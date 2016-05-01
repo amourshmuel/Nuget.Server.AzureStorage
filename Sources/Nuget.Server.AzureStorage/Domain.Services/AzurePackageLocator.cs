a096b1d15233266852c254feb083ecdbc84fa6cf
amourshmuel <amour.shmuel@gmail.com>
2016-05-01 19:17:40 +0300

    Naming convention
    The Blob Container name cannot contains dot/dash ( . / -)
    The Blob File in this case the Version name can and should contains dots
    ( . )

--- Sources/Nuget.Server.AzureStorage/Domain.Services/AzurePackageLocator.cs
+++ Sources/Nuget.Server.AzureStorage/Domain.Services/AzurePackageLocator.cs
@@ -6,18 +6,19 @@
     {
         public string GetContainerName(IPackage package)
         {
-            return this.GetAzureFriendlyString(NsasConstants.ContainerPrefix+package.Id);
+            return this.GetAzureFriendlyString(NsasConstants.ContainerPrefix + package.Id);
         }
 
         public string GetItemName(IPackage package)
         {
-            return this.GetAzureFriendlyString(package.Version.ToString());
+            return package.Version.ToString();
+          //  return this.GetAzureFriendlyString(package.Version.ToString());
         }
 
         private string GetAzureFriendlyString(string packageId)
         {
-            //return packageId.ToLower().Replace(".", "-").Replace("_","-");
-            return packageId.ToLower().Replace("_", "-");
+            return packageId.ToLower().Replace(".", "-").Replace("_","-");
+            //return packageId.ToLower().Replace("_", "-");
         }
     }
 }
